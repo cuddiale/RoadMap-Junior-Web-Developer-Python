38b7120b1b296c9585ef6a62ad96cfffc1253634
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-09 21:56:55 +0100

    miglioramento primo script
    ho aggiunto le tipologie di dato ad ogni variabile

--- Settimana 1 – Python Fondamenti/Script che stampa i tuoi dati personali.py
+++ Settimana 1 – Python Fondamenti/Script che stampa i tuoi dati personali.py
@@ -1,7 +1,7 @@
 # creare uno script che stampa i mie dati personali
 
-nome = 'Alessio'
-eta = 30
-altezza = 1.70
+nome = str("Alessio")
+eta = int(30)
+altezza = float(1.75)
 
 print(f'ciao mi chiamo {nome}, ho {eta} anni, sono alto {altezza}')
