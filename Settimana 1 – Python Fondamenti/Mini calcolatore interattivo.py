b133927d4f63c7968d9c8c994a149d7fa7a7fbc1
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-10 17:53:59 +0100

    Finito secondo progetto, mini calcolatore età con input

---
+++ Settimana 1 – Python Fondamenti/Mini calcolatore interattivo.py
@@ -0,0 +1,14 @@
+# in questo script ci concentreremo nel chiedere due input all'utente e calcolare l'età futura 
+
+#creato variabili con conversione degli input
+
+nome = str(input("Come ti chiami??  "))
+eta = int(input("Quanti anni hai??  ")) 
+anno = int(input("Fra quanti anni vuoi vuoi calcolare la tua età??  "))
+calcolo = int(eta + anno)
+
+print(f"Ciao {nome}, complimenti per i tuoi {eta} anni.") 
+print(f"Fra {anno} anni, avrai {calcolo} anni")
+
+
+# miglioramento possibile futuro la gestione degli errori
