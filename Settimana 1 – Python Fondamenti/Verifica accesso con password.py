469c8f1195f7dbf1034842993cbc9b8d2f09b5e3
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-11 18:28:05 +0100

    Ultimato da testare.
    inserito il programma dentro l'if iniziale.
    se la password è corretta continua il codice, se la password  è errata non entra nel codice.

--- Settimana 1 – Python Fondamenti/Verifica accesso con password.py
+++ Settimana 1 – Python Fondamenti/Verifica accesso con password.py
@@ -3,4 +3,36 @@
 # stampare Numero pari/dispari
 # stampare Maggiore/minore
 
-password = input("Inserisci la tua Password")
+password = input("Inserisci la tua Password: ")
+ripeti = input("Inserisci la tua Password: ")
+
+if password == ripeti:
+    print("La password è CORRETTA, procediamo!")
+    numero = int(input("inserisci un numero e ti dirò se pari o dispari."))
+    
+    if numero % 2 == 0:
+     print(f"Il numero {numero} è PARI")
+     
+    else:
+     print(f"Il numero {numero} è DISPARI")
+     
+    numero2 = int(input("inserisci un secondo numero e ti dirò quale dei due è maggiore dell'altro."))
+    
+    if numero > numero2 :
+        print(f"{numero} è maggiore di {numero2}!")
+        
+    elif numero == numero2:
+        print(f"I due numeri sono uguali!")
+        
+    else:
+        print(f"il {numero2} è maggiore di {numero}!")     
+    
+     
+         
+else:
+    print("La password è SBAGLIATA.")
+    
+
+
+
+
