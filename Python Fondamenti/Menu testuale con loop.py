311c50a971ce4eb59da386d6e83b0d6b13b717e4
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-13 09:40:49 +0100

    definiti gli obiettivi del programma

--- Python Fondamenti/Menu testuale con loop.py
+++ Python Fondamenti/Menu testuale con loop.py
@@ -1,1 +1,7 @@
-#  Menu testuale con loop, creare una lista e stampare questa lista nel ciclo 
+# Menu testuale con loop, creare una lista e stampare questa lista nel ciclo 
+
+# Stampa numeri 1–10
+# Somma numeri stampati
+
+
+
