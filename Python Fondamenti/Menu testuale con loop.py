a8f1d6708a016e433195f2ca21a1a811c8ba78f6
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-13 11:02:28 +0100

    update

--- Python Fondamenti/Menu testuale con loop.py
+++ Python Fondamenti/Menu testuale con loop.py
@@ -3,5 +3,31 @@
 # Stampa numeri 1–10
 # Somma numeri stampati
 
+# for
+# while
+# range()
+
+menu = ["pizza", "pasta", "carne", "pesce"]
+
+for x in menu:
+    print(x)
+    
+# stampare il primo e il secondo elemento della lista
+
+for y in menu:
+    print(y[:2])
+    
+# stampare numeri da 1 a 10
+    
+for i in range(10):
+    print(i)
+    
+# stampiamo i numeri da 1 a 10 col ciclo while
+i= 0
+while i <11:
+    print(i)
+    i+=1
 
 
+
+    
