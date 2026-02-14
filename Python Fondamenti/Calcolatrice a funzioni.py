dbff7a9eb803547a897915c741f38db82a8105e5
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-14 19:15:14 +0100

    inizio progetto di calcolatrice

--- Python Fondamenti/Calcolatrice a funzioni.py
+++ Python Fondamenti/Calcolatrice a funzioni.py
@@ -2,7 +2,36 @@
 # Parametri
 # return
 
+
 # Funzione somma
-# Funzione media
+# Funzione sottrazione
+# Funzione prodotto
+# Funzione divisione
+
+# definisco le funzioni
+
+def somma(a, b):
+    return a + b
+
+def sottrazione(a, b):
+    return a - b
+
+def moltiplicazione(a, b):
+    return a * b
+
+def divisione(a, b):
+    if b == 0:
+        return "Errore: divisione per zero!"
+    return a / b
+
+
+# creare ciclo infinito e scelta utente cosa vuole fare
+
+
+
+
+
+numero = int(input("Inserisci un numero: "))
+numero2 = int(input("Inserisci un numero: "))
 
 
