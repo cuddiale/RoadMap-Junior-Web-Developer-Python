04730307b0e71218c723c0ed0c337168eb5d12db
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-15 19:33:13 +0100

    creato progetto confronto fra due numeri

---
+++ Python Fondamenti/Confronto tra due numeri.py
@@ -0,0 +1,14 @@
+
+while True:
+    
+    numero = float(input("Inserisci il primo numero "))
+    numero_2 = float(input("Inserisci il secondo numero "))
+
+    if numero > numero_2:
+        print(f"il {numero} è maggiore di {numero_2} ") 
+           
+    elif numero == numero_2:
+        print(f"il {numero} e il {numero_2} sono uguali") 
+        
+    elif numero < numero_2:
+        print(f"il {numero} è minore di {numero_2} ") 
