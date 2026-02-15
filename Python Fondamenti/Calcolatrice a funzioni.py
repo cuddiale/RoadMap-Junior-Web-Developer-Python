a3d267d93083a558c33ea2f3c99ac1aa432e1b2c
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-15 14:32:06 +0100

    progetto calcolatrice finito

--- Python Fondamenti/Calcolatrice a funzioni.py
+++ Python Fondamenti/Calcolatrice a funzioni.py
@@ -1,6 +1,6 @@
 
 import unittest
-
+import time
 # definisco le funzioni
 
 # Funzione somma
@@ -35,34 +35,43 @@ while True:
     print("4 - Divisione")
     print("0 - Uscire \n")
 
-    scelta=str(input("Inserisci la tua scelta, [1] [2] [3] [4] per le operazioni oppure premi [0] per uscire. ")).lower
-    
-    if scelta == "1" or scelta == "2" or scelta == "3" or scelta == "4" or scelta == "0":
-        numero = str(input("Inserisci il primo numero: "))
-        numero_2 = str(input("Inserisci il secondo numero: "))
-              
-  
-        if scelta == "0":
+    scelta= str(input("Inserisci la tua scelta, [1] [2] [3] [4] per le operazioni oppure premi [0] per uscire. "))
+   
+    if scelta == "1" or scelta == "2" or scelta == "3" or scelta == "4":
+        
+        numero = int(input("Inserisci il primo numero: "))
+        numero_2 = int(input("Inserisci il secondo numero: "))
+                              
+        if scelta == "1":
+            print("Il risultato della somma è:",somma(numero, numero_2))
+            time.sleep(1)
+            print("")
+        elif scelta == "2":
+            print("Il risultato della sottrazione è:",sottrazione(numero, numero_2))
+            time.sleep(1)
+            print("")
+        elif scelta == "3":
+            print("Il risultato della moltiplicazizone è:",moltiplicazione(numero, numero_2))
+            time.sleep(1)
+            print("")
+        elif scelta == "4":
+            print("Il risultato della divisione è:",divisione(numero, numero_2))
+            time.sleep(1)
+            print("")
+        else:
+            time.sleep(1)
+            continue        
+            
+    elif scelta == "0":
             uscita_loop = input("Confermi di uscire? Y o N ").lower()
             if uscita_loop == "y":    #inserito una doppia conferma per l'uscita
                     print("Il programma si sta chiudendo!")
                     print("Grazie per aver usato la calcolatrice!")
                     break   
             else:
-                    continue     
-                
-        elif scelta == "1":
-            print("Il risultato della somma è:",somma(numero, numero_2))
-    
-        elif scelta == "2":
-            print("Il risultato della sottrazione è:",sottrazione(numero, numero_2))
-         
-        elif scelta == "3":
-            print("Il risultato della moltiplicazizone è:",moltiplicazione(numero, numero_2))
-         
-        elif scelta == "4":
-         print("Il risultato della divisione è:",divisione(numero, numero_2))
+                continue  
 
     else:
-        print("Operazione non valida.")
+        print("Operazione non valida.\n")
+        time.sleep(2)
         continue
