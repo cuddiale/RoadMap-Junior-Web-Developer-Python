00cc31bb2004b55c7ef2dac080f835f303536897
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-15 13:28:42 +0100

    correggere i bug

--- Python Fondamenti/Calcolatrice a funzioni.py
+++ Python Fondamenti/Calcolatrice a funzioni.py
@@ -1,14 +1,13 @@
-# def
-# Parametri
-# return
 
+import unittest
+
+# definisco le funzioni
 
 # Funzione somma
 # Funzione sottrazione
 # Funzione prodotto
 # Funzione divisione
 
-# definisco le funzioni
 
 def somma(a, b):
     return a + b
@@ -34,51 +33,36 @@ while True:
     print("2 - Sottrazione")
     print("3 - Moltiplicazione")
     print("4 - Divisione")
-    print("0 - Uscire")
+    print("0 - Uscire \n")
 
-    scelta=str(input("Inserisci la tua scelta, [1] [2] [3] [4] per le operazioni oppure [0] per uscire "))
+    scelta=str(input("Inserisci la tua scelta, [1] [2] [3] [4] per le operazioni oppure premi [0] per uscire. ")).lower
     
-    if scelta == "0":
-        uscire = input("Confermi di uscire? Y o N ").lower()
-        if uscire == "y":    #inserito una doppia conferma per l'uscita
-            print("Il programma si sta chiudendo!")
-            print("Grazie per aver usato la calcolatrice!")
-            break   
-        else:
-            continue
-        
-    # scelta numeri utente
+    if scelta == "1" or scelta == "2" or scelta == "3" or scelta == "4" or scelta == "0":
+        numero = str(input("Inserisci il primo numero: "))
+        numero_2 = str(input("Inserisci il secondo numero: "))
+              
   
+        if scelta == "0":
+            uscita_loop = input("Confermi di uscire? Y o N ").lower()
+                if uscita_loop == "y":    #inserito una doppia conferma per l'uscita
+                    print("Il programma si sta chiudendo!")
+                    print("Grazie per aver usato la calcolatrice!")
+                    break   
+                else:
+                    continue     
+                
+        elif scelta == "1":
+            print("Il risultato della somma è:",somma(numero, numero_2))
     
-# ============================
-# TODO: CAMBIARE QUI LA LOGICA vorrei che l'utente se non inserisce 1234 non inserisca i numeri da operare.
-# ============================
-
-    if  scelta != "1":
-        
-        numero = float(input("Inserisci il primo numero: "))
-        numero_2 = float(input("Inserisci il secondo numero: "))  
-        continue    
-        
-        
-             
-    elif scelta == "1":
-        print("Il risultato della somma è:",somma(numero, numero_2))
-    
-    elif scelta == "2":
-         print("Il risultato della sottrazione è:",sottrazione(numero, numero_2))
+        elif scelta == "2":
+            print("Il risultato della sottrazione è:",sottrazione(numero, numero_2))
          
-    elif scelta == "3":
-         print("Il risultato della moltiplicazizone è:",moltiplicazione(numero, numero_2))
+        elif scelta == "3":
+            print("Il risultato della moltiplicazizone è:",moltiplicazione(numero, numero_2))
          
-    elif scelta == "4":
+        elif scelta == "4":
          print("Il risultato della divisione è:",divisione(numero, numero_2))
 
     else:
-        print("Scelta non valida!")
-
-
-
-       
-
-    
+        print("Operazione non valida.")
+        continue
