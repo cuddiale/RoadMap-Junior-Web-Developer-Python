3517f4a2c47f4520d587dcdb1c9cd0c42bab4845
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-02-16 21:51:11 +0100

    aggiornamento esercizio

--- Settimana 2 – Strutture Dati & File/Gestione lista spesa.py
+++ Settimana 2 – Strutture Dati & File/Gestione lista spesa.py
@@ -1,2 +1,50 @@
 # crare una lista della spesa con input utente
 # creare anche logica del calcellare se esiste e aggiungere se non esiste
+
+
+# Indicizzazione
+
+numeri = [10, 20, 30]
+
+print(numeri[0])  # 10
+print(numeri[1])  # 20
+
+
+# Somma e media
+
+numeri = [5, 10, 15, 20, 25]
+
+somma = sum(numeri)
+media = somma / len(numeri)
+
+print("Somma:", somma)
+print("Media:", media)
+
+
+# Ricerca elemento
+
+numeri = [5, 10, 15, 20, 25]
+
+elemento = 15
+
+if elemento in numeri:
+    print("Elemento trovato!")
+else:
+    print("Elemento non trovato.")
+
+
+# Gestione Lista Spesa con input utente
+
+spesa = []
+
+while True:
+    prodotto = input("Inserisci prodotto (o 'fine' per uscire): ")
+    
+    if prodotto == "fine":
+        break
+    
+    spesa.append(prodotto)
+
+print("La tua lista della spesa:")
+for item in spesa:
+    print("-", item)
