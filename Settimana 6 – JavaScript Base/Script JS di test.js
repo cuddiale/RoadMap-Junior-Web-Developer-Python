b8c92d10fcdb2346b1cb18c9c7770de67eb69f52
alessio cuddretto <alessiocuddretto@hotmail.com>
2026-03-10 16:44:01 +0100

    aggiunto esercizio sulle variabili in Js

---
+++ Settimana 6 – JavaScript Base/Script JS di test.js
@@ -0,0 +1,17 @@
+// 1. Dichiarazione variabili
+let nome = "Marco";
+let eta = 25;
+let studente = true;
+let altezza = 1.75;
+
+// 2. Stampa dei valori
+console.log("Nome:", nome);
+console.log("Età:", eta);
+console.log("Studente:", studente);
+console.log("Altezza:", altezza);
+
+// 3. Verifica tipi di dato
+console.log("Tipo di nome:", typeof nome);
+console.log("Tipo di eta:", typeof eta);
+console.log("Tipo di studente:", typeof studente);
+console.log("Tipo di altezza:", typeof altezza);
