0e5d152faab37f5c044560190011bfc27a6013b0
Aless <alessiocuddretto@hotmail.com>
2026-06-10 17:39:09 +0200

    riordine

--- Settimana 9 – Flask + Database/Esercitazione SQL/Traccia esercitazione.sql
+++ Settimana 9 – Flask + Database/Esercitazione SQL/Traccia esercitazione.sql
@@ -1,17 +1,47 @@
 
 -- 01 tutti i docenti
+
+
 -- 02 email studenti maggiorenni
+
+
 -- 03 corsi già terminati
+
+
 -- 04 prossimi corsi in programma
+
+
 -- 05 prossim corsi che costano meno di [scegliere cifral
+
+
 -- 06 tutti i corsi di un docentel
+
+
 -- 07 tutti i corsi passati di un docente
+
+
 -- 08 numero studenti iscritti ad un particolare corso
+
+
 -- 09 corsi che hanno raggiunto la capacità massima
+
+
 -- 10 numero di corsi frequentati da uno studente
+
+
 -- 11 lista corsi frequentati da uno studente
+
+
 -- 12 calcolare quante volte i corsi sono stati riproposti
+
+
 -- 13 l'insegnante con più corsi insegnati
+
+
 -- 14 top 3 studenti più partecipi
+
+
 -- 15 il primissimo corso che abbiamo rilasciato
+
+
 -- 16 lista studenti che hanno partecipato al corso di un insegnante specifico
